afe32c0d353d25b4b9c17809c12e630176f83cf9
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-30 23:43:53 +0600

    Change the server url

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    // baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://blood-bank-server-side-4nepatwp6-md-rakib-hassans-projects.vercel.app',
+
 
     withCredentials:true,
   });
